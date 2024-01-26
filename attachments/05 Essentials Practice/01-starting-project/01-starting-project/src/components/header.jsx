a7de837fe08f5dd3d279dcf6994b7834da92b7dd
Tim van der Weijde <tim@bluecape.nl>
2024-01-26 17:17:00 +0100

    remove folder again

--- attachments/05 Essentials Practice/01-starting-project/01-starting-project/src/components/header.jsx
+++
@@ -1,8 +0,0 @@
-export default function Header() {
-  return (
-    <div id="header">
-      <img src="investment-calculator-logo.png" alt="logo" />
-      <h1> Investment Calculator</h1>
-    </div>
-  );
-}
