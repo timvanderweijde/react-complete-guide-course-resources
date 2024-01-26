c68c9c3616c9140f8f6fb9d17d11caa7509edd26
Tim van der Weijde <tim@bluecape.nl>
2024-01-26 17:39:30 +0100

    Conditional added

--- attachments/05 Essentials Practice/01-starting-project/src/App.jsx
+++ attachments/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -5,25 +5,30 @@ import { useState } from "react";
 import { calculateInvestmentResults } from "./util/investment.js";
 
 function App() {
-
   const INITAL_INVESTMENT = {
     initialInvestment: 1000,
     annualInvestment: 1200,
     expectedReturn: 6,
-    duration: 10,
-    result: []
-  }
-
-  let [investment, setInvestment] = useState(INITAL_INVESTMENT);
+    duration: 0,
+    result: [],
+  };
+  
+  const [investment, setInvestment] = useState(INITAL_INVESTMENT);
+  const [isInputValid, setIsInputValid] = useState(false);
 
   function updateInvestment(key, value) {
-
     let newInvestment = { ...investment };
 
     newInvestment[key] = Number(value);
 
-    let result = calculateInvestmentResults(newInvestment);
-    newInvestment.result = result;
+    setIsInputValid(newInvestment.duration >= 1);
+
+    if (newInvestment.duration >= 1) {
+      let result = calculateInvestmentResults(newInvestment);
+      newInvestment.result = result;
+    } else {
+      newInvestment.result = [];
+    }
 
     setInvestment(newInvestment);
   }
@@ -31,10 +36,17 @@ function App() {
   return (
     <div>
       <Header />
-      <Input investmentValue={investment}
+      <Input
+        investmentValue={investment}
         onUpdateInvestment={(key, newValue) => updateInvestment(key, newValue)}
       />
-      <Result initialInvestment={investment.initialInvestment} results={investment.result} />
+      {!isInputValid && <p className="center">Please fill in duration.</p>}
+      {isInputValid && 
+        <Result
+          initialInvestment={investment.initialInvestment}
+          results={investment.result}
+        />
+      }
     </div>
   );
 }
--- attachments/05 Essentials Practice/01-starting-project/src/components/input.jsx
+++ attachments/05 Essentials Practice/01-starting-project/src/components/input.jsx
@@ -7,19 +7,19 @@ export default function Input({
       <div className="input-group">
         <p>
           <label>Initial investment:</label>
-          <input value={investmentValue.initialInvestment} onChange={(event) => onUpdateInvestment('initialInvestment', event.target.value)} />
+          <input type="number" required value={investmentValue.initialInvestment} onChange={(event) => onUpdateInvestment('initialInvestment', event.target.value)} />
         </p>
         <p>
           <label>Annual investment:</label>
-          <input value={investmentValue.annualInvestment} onChange={(event) => onUpdateInvestment('annualInvestment', event.target.value)} />
+          <input type="number" required value={investmentValue.annualInvestment} onChange={(event) => onUpdateInvestment('annualInvestment', event.target.value)} />
         </p>
       </div>
       <div className="input-group">
         <p><label>Expected return:</label>
-          <input value={investmentValue.expectedReturn} onChange={(event) => onUpdateInvestment('expectedReturn', event.target.value)}/>
+          <input type="number" required value={investmentValue.expectedReturn} onChange={(event) => onUpdateInvestment('expectedReturn', event.target.value)}/>
         </p>
         <p> <label>Duration:</label>
-          <input value={investmentValue.duration} onChange={(event) => onUpdateInvestment('duration', event.target.value)}/>
+          <input type="number" required value={investmentValue.duration} onChange={(event) => onUpdateInvestment('duration', event.target.value)}/>
         </p>
       </div>
     </section>
